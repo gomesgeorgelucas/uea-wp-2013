d1cdb8535dd2a9ae3f842275bce5a0bfe5ee9235
Walderi Moraes Willy Filho <walderif@hotmail..com>
2013-10-03 15:29:39 -0400

    [DISPLAYACOMPANHAMENTO]

--- DirectCalc7.1/DirectCalc7.1/DCBasica.xaml.cs
+++ DirectCalc7.1/DirectCalc7.1/DCBasica.xaml.cs
@@ -13,36 +13,47 @@ namespace DirectCalc7
 {
     public partial class DCBasica : PhoneApplicationPage
     {
-        List<char> calculo = new List<char>();
-        StringBuilder cal = new StringBuilder();
+        /// <summary>
+        ///     Interface da calculadora
+        /// </summary>
+      // propriedade do diaplay da calculadora
+        private String _displayMsg;
+      
+	    public String displayMsg
+	    {
+	    	get { return _displayMsg;}
+	    	set {
+                    _displayMsg = value;
+            }
+	    }
         
         public DCBasica()
+        ///Inicializador da calculadora
         {
             InitializeComponent();
         }
-        private void mostrarCalc()
+
+        private void mostrarCalc(String displayMsg)
         {
-            char
-            c = calculo.Last<char>();
-            cal.Append(c).Append("");
-            
-            string s = string.Join("",calculo.ToArray());
-            txtExpressao.Text = s;
+        /// mostra a string no display de acompanhamento da expressão
+            txtExpressao.TextWrapping = TextWrapping.Wrap;//evita que a string ulttrapasse o textBlock
+            txtExpressao.Visibility = System.Windows.Visibility.Visible;//mostra o exedente da string abaixo
+            txtExpressao.TextAlignment = TextAlignment.Right;//alinha a string a direita do textBlock
+            txtExpressao.Text = displayMsg;//mostra o que tem na string no textBlock
         }
+
         private void bt0_Click(object sender, RoutedEventArgs e)
         {
-            calculo.Add('0');
-           /* s = string.Join("",calculo.ToArray());
-            txtExpressao.Text = s;*/
-            mostrarCalc();
+            ///Botão para o número 0
+            displayMsg = displayMsg + "0";
+            mostrarCalc(displayMsg);
         }
 
         private void btPt_Click(object sender, RoutedEventArgs e)
         {
-            calculo.Add('.');
-           /* s = string.Join("", calculo.ToArray());
-            txtExpressao.Text = s;*/
-            mostrarCalc();
+            ///Botão para o ponto
+            displayMsg = displayMsg + ".";
+            mostrarCalc(displayMsg);
         }
 
         private void btIgual_Click(object sender, RoutedEventArgs e)
@@ -52,123 +63,114 @@ namespace DirectCalc7
 
         private void bt1_Click(object sender, RoutedEventArgs e)
         {
-            calculo.Add('1');
-            /*s = string.Join("", calculo.ToArray());
-            txtExpressao.Text = s;
-*/
-            mostrarCalc();
+            ///Botão para o número 1
+            displayMsg = displayMsg + "1";
+            mostrarCalc(displayMsg);
         }
 
         private void bt2_Click(object sender, RoutedEventArgs e)
         {
-            calculo.Add('2');
-           /* s = string.Join("", calculo.ToArray());
-            txtExpressao.Text = s;*/
-            mostrarCalc();
+            ///Botão para o número 2
+            displayMsg = displayMsg + "2";
+            mostrarCalc(displayMsg);
         }
 
         private void bt3_Click(object sender, RoutedEventArgs e)
         {
-            calculo.Add('3');
-            /*s = string.Join("", calculo.ToArray());
-            txtExpressao.Text = s;*/
-            mostrarCalc();
+            ///Botão para o número 3
+            displayMsg = displayMsg + "3";
+            mostrarCalc(displayMsg);
         }
 
         private void btDiv_Click(object sender, RoutedEventArgs e)
         {
-            calculo.Add('/');
-            /*s = string.Join("", calculo.ToArray());
-             txtExpressao.Text = s;*/
-            mostrarCalc();
+            ///Botão para o operador de divisão
+            displayMsg = displayMsg + "/";
+            mostrarCalc(displayMsg);
         }
 
         private void bt4_Click(object sender, RoutedEventArgs e)
         {
-            calculo.Add('4');
-            /*s = string.Join("", calculo.ToArray());
-                txtExpressao.Text = s;*/
-            mostrarCalc();
+            ///Botão para o número 4
+            displayMsg = displayMsg + "4";
+            mostrarCalc(displayMsg);
         }
 
         private void bt5_Click(object sender, RoutedEventArgs e)
         {
-            calculo.Add('5');
-            /*s = string.Join("", calculo.ToArray());
-                txtExpressao.Text = s;*/
-            mostrarCalc();
+            ///Botão para o número 5
+            displayMsg = displayMsg + "5";
+            mostrarCalc(displayMsg);
         }
 
         private void bt6_Click(object sender, RoutedEventArgs e)
         {
-            calculo.Add('6');
-            /*s = string.Join("", calculo.ToArray());
-                txtExpressao.Text = s;*/
-            mostrarCalc();
+            ///Botão para o número 6
+            displayMsg = displayMsg + "6";
+            mostrarCalc(displayMsg);
         }
 
         private void btMult_Click(object sender, RoutedEventArgs e)
         {
-            calculo.Add('x');
-            /*s = string.Join("", calculo.ToArray());
-                txtExpressao.Text = s;*/
-            mostrarCalc();
+            ///Botão para o operador de multiplicação
+            displayMsg = displayMsg + "x";
+            mostrarCalc(displayMsg);
         }
 
         private void bt7_Click(object sender, RoutedEventArgs e)
         {
-            calculo.Add('7');
-            /*s = string.Join("", calculo.ToArray());
-                txtExpressao.Text = s;*/
-            mostrarCalc();
+            ///Botão para o número 7
+            displayMsg = displayMsg + "7";
+            mostrarCalc(displayMsg);
         }
 
         private void bt8_Click(object sender, RoutedEventArgs e)
         {
-            calculo.Add('8');
-            /*s = string.Join("", calculo.ToArray());
-                txtExpressao.Text = s;*/
-            mostrarCalc();
+            ///Botão para o número 8
+            displayMsg = displayMsg + "8";
+            mostrarCalc(displayMsg);
         }
 
         private void bt9_Click(object sender, RoutedEventArgs e)
         {
-            calculo.Add('9');
-            /*s = string.Join("", calculo.ToArray());
-                txtExpressao.Text = s;*/
-            mostrarCalc();
+            ///Botão para o número 9
+            displayMsg = displayMsg + "9";
+            mostrarCalc(displayMsg);
         }
 
         private void btMin_Click(object sender, RoutedEventArgs e)
         {
-            calculo.Add('-');
-            /*s = string.Join("", calculo.ToArray());
-                txtExpressao.Text = s;*/
-            mostrarCalc();
+            ///Botão para o operador de subtração
+            displayMsg = displayMsg + "-";
+            mostrarCalc(displayMsg);
         }
 
         private void btAbrepar_Click(object sender, RoutedEventArgs e)
         {
-            calculo.Add('(');
-            /*s = string.Join("", calculo.ToArray());
-                txtExpressao.Text = s;*/
-            mostrarCalc();
+            ///Botão para o caractere de prioridade abre parenteses
+            displayMsg = displayMsg + "(";
+            mostrarCalc(displayMsg);
         }
 
         private void btFechapar_Click(object sender, RoutedEventArgs e)
         {
-            calculo.Add(')');
-            /*s = string.Join("", calculo.ToArray());
-                txtExpressao.Text = s;*/
-            mostrarCalc();
+            ///Botão para o caractere de prioridade fecha parenteses
+            displayMsg = displayMsg + ")";
+            mostrarCalc(displayMsg);
         }
 
         private void btMais_Click(object sender, RoutedEventArgs e)
         {
-            calculo.Add('+');
-            /*s = string.Join("", calculo.ToArray());
-                txtExpressao.Text = s;*/
-            mostrarCalc();
+            ///Botão para o operador de adição
+            displayMsg = displayMsg + "+";
+            mostrarCalc(displayMsg);
+        }
+
+        private void btAC_Click(object sender, RoutedEventArgs e)
+        {
+            ///Botão para apagar o ultimo acrescimo a string
+            displayMsg = displayMsg.Remove(displayMsg.Length - 1);
+            mostrarCalc(displayMsg);
         }
 
     }
